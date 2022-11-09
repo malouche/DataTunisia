24f9c3dcde354a02c258689b537874d315c6c1d3
malouche <dhafer.malouche@qu.edu.qa>
2022-11-09 12:41:59 +0000

    ok

--- code.R
+++ code.R
@@ -1,2 +1,5 @@
 ## tab217: 2.1.7 Population scolaire par genre et par gouvernorat
 ## tab2112: 2.1.12 Population scolaire par genre et par gouvernorat
+
+library(readxl)
+data_schools_INS <- read_excel("data_schools_INS.xlsx")
